2f55850612f45256858dd801da088590cf140f2d
Jorge Iglesias Roldan <trabajo.jorge2000@gmail.com>
2022-06-22 23:43:59 +0200

    Commit 22-06-22

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { Component } from '@angular/core';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   title = 'Entrega2';
+  constructor(){}
+
+  miFormulario= new FormGroup({
+    userEmail : new FormControl('', Validators.email),
+    userPass : new FormControl('', Validators.required)
+  });
+
 }
